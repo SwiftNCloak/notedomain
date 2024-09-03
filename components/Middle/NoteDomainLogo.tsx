0ef9239e7ba4ca947a0d0cb0c632c49f53c50eb1
SwiftNCloak <markyjamesbonifacio@gmail.com>
2024-09-03 22:32:27 +0800

    changed icon of domainlogo hehe

--- components/Middle/NoteDomainLogo.tsx
+++ components/Middle/NoteDomainLogo.tsx
@@ -1,9 +1,12 @@
 "use client"
 
+import { BookOpenText } from "lucide-react";
+
 export default function NoteDomainLogo() {
   return (
     <div className="flex items-center justify-center p-2 w-12 max-w-12 box-border h-12 max-h-12 border border-[#2b2b2bd9] bg-[#e8e8e829] dark:bg-[#2b2b2bd9] text-[#b7b7b7ee] hover:bg-[#00D166] hover:text-[#e8e8e8] rounded-2xl cursor-pointer transition ease-in-out">
-        <h1 className="font-black text-xl truncate">ND</h1>
+        {/* <h1 className="font-black text-xl truncate">ND</h1> */}
+        <BookOpenText size={25} />
     </div>
   );
 }
