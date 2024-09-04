5445e25e7aca25b27b99838e39e5949dfab32176
SwiftNCloak <markyjamesbonifacio@gmail.com>
2024-09-04 16:20:53 +0800

    i made a working system!

--- components/Middle/Notebook.tsx
+++ components/Middle/Notebook.tsx
@@ -1,12 +1,15 @@
 "use client"
 
-import { NotebookText } from "lucide-react";
+import { NotebookText, FilePlus } from "lucide-react";
 
 export default function Notebook() {
   return (
-    <div className="w-full box-border h-9 max-h-9 hover:bg-[#2b2b2bd9] rounded-lg overflow-y-auto px-3 items-center justify-start flex space-x-2 font-semibold">
-      <NotebookText size={17} className="text-[#9f9f9fd9]"/>
-      <p className="text-[#9f9f9fd9] text-sm">Notebook</p>
+    <div className="group w-full box-border h-9 max-h-9 hover:bg-[#2b2b2bd9] rounded-lg overflow-y-auto px-3 items-center justify-between flex space-x-2 font-semibold">
+      <div className="flex space-x-2 items-center justify-start">
+        <NotebookText size={17} className="text-[#9f9f9fd9]"/>
+        <p className="text-[#9f9f9fd9] text-sm">Notebook</p>
+      </div>
+      <FilePlus size={17} className="text-[#9f9f9fd9] opacity-0 group-hover:opacity-100"/>
     </div>
   );
 }
